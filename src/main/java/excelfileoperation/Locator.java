c126405f225f7ec8c0e7525bf77d1f1af5dc10ac
AbdhullaThalif <thalifabdhul.101@gamil.com>
2024-03-11 10:10:42 +0530

    First Comment

--- src/main/java/excelfileoperation/Locator.java
+++ src/main/java/excelfileoperation/Locator.java
@@ -1,5 +1,6 @@
 package excelfileoperation;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,12 +8,17 @@ public class Locator {
 	
 	public static void main(String[] args) {
 		
+		
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.amazon.in/");
-		System.out.println("driver.getCurrentUrl()");
 		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys("Iphone");
+		driver.findElement(By.xpath("//input[@id='nav-search-submit-button']")).click();
+		//System.out.println("driver.getCurrentUrl()");
 		System.out.println(driver.getTitle());
-		System.out.println(driver.getPageSource());
-}
+		//System.out.println(driver.getPageSource());
+		
+	
+		}
 
 }
